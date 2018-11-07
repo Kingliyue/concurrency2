fee0958fe2d440c9c178bebc928db84be34d33d6
Your Name <you@example.com>
2018-11-07 15:36:18 +0800

    2018117提交的

--- src/main/java/com/mmall/concurrency/ConcurrencyApplication.java
+++ src/main/java/com/mmall/concurrency/ConcurrencyApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 public class ConcurrencyApplication extends WebMvcConfigurerAdapter{
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrencyApplication.class, args);
 	}
